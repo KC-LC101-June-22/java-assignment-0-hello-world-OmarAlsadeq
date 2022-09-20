9feed6077dd0eaad8edff2ae8159dc6591feabd7
OmarAlsadeq <109781174+OmarAlsadeq@users.noreply.github.com>
2022-09-20 17:07:53 -0500

    Solves assignment 0

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,7 @@
 /**
  * Created by LaunchCode
  */
+import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
@@ -8,7 +9,10 @@ public class HelloWorld {
     }
 
     public static String sayHello() {
-        return "";
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        return "Hello, World!";
     }
 
 }
